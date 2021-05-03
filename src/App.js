2b3adea484bb44e24f614f6b5f65a07015f09702
isthatsahil <sahil.verma997@gmail.com>
2021-05-04 00:48:12 +0530

    setup complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'App.css';
+import Nav from './Nav';
+
+const App = () => {
+
+    return(
+        <div>
+            <Nav/>
+            <h1 className="wow">Hello React !!</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#About">About</a>
+                    </li>
+                </ul>    
+            </nav>      
+        </div>
+    );
+}
+
+export default Nav;
